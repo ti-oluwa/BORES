8626ae195608969f3d54c37771c12ba544683011
ti-oluwa <tioluwa.dev@gmail.com>
2026-09-22 22:01:15 +0100

    delete some old modules

--- src/bores/helpers.old/__init__.py
+++
@@ -1,6 +0,0 @@
-"""APIs and utilities for building N-Dimensional cartesian grids for reservoir simulation."""
-
-from .base import *  # noqa
-from .utils import *
-from .saturation import *
-from .pressure import *
